6b796c9efdc1a2cca82db90f3465739b051bd17e
Dennis Flagg <deflagg@hotmail.com>
2026-01-19 21:55:40 -0500

    Add ROOT_DIR setup to circle_manifold.py for improved module accessibility

--- analysis/circle_manifold.py
+++ analysis/circle_manifold.py
@@ -2,6 +2,10 @@ import argparse
 import os
 import sys
 
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
+
 import numpy as np
 import torch
 
